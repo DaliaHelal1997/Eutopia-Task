5c4754666ed8a72423146f9130168375a7ffafd5
daliahelal1997 <36996176+DaliaHelal1997@users.noreply.github.com>
2021-01-29 18:27:23 +0200

    Modifications to the existing files

--- Eutopia task/js/script.js
+++ Eutopia task/js/script.js
@@ -3,6 +3,31 @@
 	const inputBox = searchWrapper.querySelector("input");
 	const suggBox = searchWrapper.querySelector(".autocom-box");
 
+	var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+  	modal.style.display = "block";
+  	btn.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+  		modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+  		if (event.target == modal) {
+    		modal.style.display = "none";
+  }
+}
 	//if user presses any key and release
 	inputBox.onkeyup = (e)=>{
 		let userData = e.target.value;
@@ -69,29 +94,5 @@
   	
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 	
